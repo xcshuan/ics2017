9b13689eaa4b532d0542457ec90032f811cb9e7e
tracer-ics2017 <tracer@njuics.org>
2018-07-22 23:02:14 +0800

    >  compile 161221076 xcshuan Linux debian 4.9.0-6-686-pae #1 SMP Debian 4.9.82-1+deb9u3 (2018-03-02) i686 GNU/Linux  23:02:14 up  3:29,  2 users,  load average: 0.00, 0.02, 0.04 b0c8a943434696044b04013f82f46e28325fe186

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,22 +14,30 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
-  struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
-
-  /* Do NOT change the order of the GPRs' definitions. */
-
-  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
+// define the structure of registers
+typedef struct
+{
+    // general purpose registers
+    union {
+        union {
+            union {
+                uint32_t _32;
+                uint16_t _16;
+                uint8_t _8[2];
+            };
+            uint32_t val;
+        } gpr[8];
+        struct
+        { // do not change the order of the registers
+            uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+        };
+    };
+    /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
   vaddr_t eip;
-
+    // EFLAGS ...
 } CPU_state;
 
 extern CPU_state cpu;
