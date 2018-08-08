e306fda0e4c038ce00b586efc2d3884dbcea1938
tracer-ics2017 <tracer@njuics.org>
2018-08-08 17:02:46 +0800

    >  compile 161221076 xcshuan Linux debian 4.9.0-6-686-pae #1 SMP Debian 4.9.82-1+deb9u3 (2018-03-02) i686 GNU/Linux  17:02:46 up  7:47, 12 users,  load average: 0.03, 0.03, 0.00 5de90654908d8ed9a6e5faeb8a258b31a8e3eaa6

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -273,7 +273,6 @@ make_DHelper(a2O) {
 make_DHelper(J) {
   decode_op_SI(eip, id_dest, false);
   // the target address can be computed in the decode stage
-  printf("JJJJ\n");
   decoding.jmp_eip = id_dest->simm + *eip;
 }
 
