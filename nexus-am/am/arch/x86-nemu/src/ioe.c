593e3267f5caa88ffdae31b53f09b7e4ccde3e65
tracer-ics2017 <tracer@njuics.org>
2018-08-16 12:47:25 +0800

    >  run 161221076 xcshuan Linux debian 4.9.0-6-686-pae #1 SMP Debian 4.9.82-1+deb9u3 (2018-03-02) i686 GNU/Linux  12:47:25 up  3:35,  5 users,  load average: 0.56, 1.00, 0.90 b0174a0e2ac2e0554292f7cc3367b4772e7ab097

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -4,6 +4,7 @@
 #define RTC_PORT 0x48   // Note that this is not standard
 #define I8042_DATA_PORT 0x60
 #define I8042_STATUS_PORT 0x64
+#define I8042_STATUS_HASKEY_MASK 0x1
 static unsigned long boot_time;
 
 void _ioe_init() {
@@ -28,22 +29,18 @@ void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
   //for (i = 0; i < _screen.width * _screen.height; i++) {
     //fb[i] = i;
   //}
- /* int col, row;
+  int col, row;
   for(row = y;row <= y + h;row++)
 	  for(col = x;col <= x + w; col++)
 		  fb[col+row*_screen.width] = pixels[(row - y)*w + (col - x)];
-		  */
-	int i;
-	for(i = 0;i<h;i++){
-		memcpy(fb+(y+i)*_screen.width+x,pixels+i*w,w+4);
-	}
+		  
 }
 
 void _draw_sync() {
 }
 
 int _read_key() {
-	uint8_t impresskey = inb(I8042_STATUS_PORT);
+	uint8_t impresskey = inb(I8042_STATUS_PORT) & I8042_STATUS_HASKEY_MASK; 
 	if(impresskey){
 		return inl(I8042_DATA_PORT);
 	}
