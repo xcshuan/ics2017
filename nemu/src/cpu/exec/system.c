e4faefc1ce6fbc5cb46eca28c7d48ca3d37b2f95
tracer-ics2017 <tracer@njuics.org>
2018-08-20 00:29:16 +0800

    >  compile 161221076 xcshuan Linux debian 4.9.0-6-686-pae #1 SMP Debian 4.9.82-1+deb9u3 (2018-03-02) i686 GNU/Linux  00:29:16 up 13:45, 15 users,  load average: 0.14, 0.15, 0.13 ab2695aaf64cdee1bd9eed2246288e02bfaa6a5

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -6,9 +6,9 @@ void diff_test_skip_nemu();
 make_EHelper(lidt) {
 	cpu.idtr.limit = vaddr_read(id_dest->addr,2);
 	if(decoding.is_operand_size_16)
-		cpu.idtr.base = vaddr_read(id_dest->val + 2, 3);
+		cpu.idtr.base = vaddr_read(id_dest->addr + 2, 3);
 	else
-		cpu.idtr.base = vaddr_read(id_dest->val + 2, 4);
+		cpu.idtr.base = vaddr_read(id_dest->addr + 2, 4);
 
   print_asm_template1(lidt);
 }
