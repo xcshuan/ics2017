c6d43cf6a98af20fca1beee8a83f766a5bd13f42
tracer-ics2017 <tracer@njuics.org>
2018-09-02 20:06:10 +0800

    >  run 161221076 xcshuan Linux debian 4.9.0-6-686-pae #1 SMP Debian 4.9.82-1+deb9u3 (2018-03-02) i686 GNU/Linux  20:06:10 up  6:56, 13 users,  load average: 0.27, 0.31, 0.28 9fdc05df33bfc775d42d4ca3ffe11e06634cab75

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,13 @@
 #include "common.h"
 #include "syscall.h"
 
-ssize_t sys_write(int fd, const void *buf, size_t count){
+ssize_t _sys_write(int fd, const void *buf, size_t count){
+	Log("sys_write!");
 	if(fd == 1 || fd == 2){
 		int i = 0;
 		while(i < count){
-			_putc(((char *)buf)[i++]);
+			_putc(((char *)buf)[i]);
+			i++;
 		}
 		return count;
 	}
@@ -24,7 +26,7 @@ _RegSet* do_syscall(_RegSet *r) {
 					break;
 	  case SYS_exit:_halt(a[1]);
 					break;
-	  case SYS_write:r->eax = sys_write(a[1],(void *)a[2],a[3]);
+	  case SYS_write:r->eax = _sys_write(a[1],(void *)a[2],a[3]);
 					 break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
