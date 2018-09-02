4a0c67e691ae88e55d46455f868ef4fc098737ea
tracer-ics2017 <tracer@njuics.org>
2018-09-02 22:18:19 +0800

    >  run 161221076 xcshuan Linux debian 4.9.0-6-686-pae #1 SMP Debian 4.9.82-1+deb9u3 (2018-03-02) i686 GNU/Linux  22:18:19 up  9:08, 15 users,  load average: 0.10, 0.17, 0.13 d4f4f636acddd912c2ab3794abdc9051d4cae659

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -32,6 +32,7 @@ _RegSet* do_syscall(_RegSet *r) {
 	  case SYS_write:r->eax = _sys_write(a[1],(void *)a[2],a[3]);
 					 break;
 	  case SYS_brk:r->eax = 0;
+				   printf("nnnnnn!\n");
 				   break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
