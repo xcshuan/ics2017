88d2dca7f38658656bcba3385b4901b74f95e101
tracer-ics2017 <tracer@njuics.org>
2018-09-06 09:55:51 +0800

    >  compile 161221076 xcshuan Linux debian 4.9.0-6-686-pae #1 SMP Debian 4.9.82-1+deb9u3 (2018-03-02) i686 GNU/Linux  09:55:51 up 53 min,  7 users,  load average: 0.44, 0.25, 0.17 8f70da9b21dddcd68fe95e15afb6f0d96cee2a3c

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -131,7 +131,7 @@ static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
 
 static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
-  *dest = ~(*dest);
+  rtl_li(dest,(*dest));
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
@@ -184,6 +184,7 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
   int zf = 0;
+  assert(width == 4 || width == 2 || width == 1);
   if(width == 1) zf = (*result & 0x000000ff) | 0;
   else if(width == 2) zf = (*result & 0x0000ffff) | 0;
   else if(width == 4) zf = (*result & 0xffffffff) | 0;
@@ -194,6 +195,7 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
    int sf = 0;
+   assert(width == 4 || width == 2 || width == 1);
    sf = (*result >> (width * 8 - 1)) & 0x1;
    cpu.eflags.SF = sf;
 }
